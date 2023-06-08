ec5e3f206a97324d098f03b79370d4ee40963e68
summereuna <summereuna@gmail.com>
2023-06-08 14:00:47 +0900

    Add sendRequestThunk

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification";
 import { useEffect } from "react";
-import { uiActions } from "./store/ui-slice";
+import { sendCartData } from "./store/cart-slice";
 
 let isInitial = true;
 
@@ -13,68 +13,15 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
   const isCartVisible = useSelector((state) => state.ui.isCartVisible);
   const cart = useSelector((state) => state.cart);
-  //useSelector는 리덕스에 대한 구독을 설정하기 때문에 리덕스 스토어가 변경될 때 마다 컴포넌트 함수를 재실행시켜 늘 최신 상태를 유지한다.
-  //따라서 여기서 가져오는 cart는 최신 버전의 카트다.
-  //그렇기 때문에 아래의 useEffect()에서 cart를 디펜던시 배열에 추가하면 cart 상태 변경 시 컴포넌트가 재렌더링 되어 최신의 상태를 가질 수 있다.
 
-  //useEffect()사용하여 카트 상태 변경 사항 관찰하기
   useEffect(() => {
-    //http request 보내기
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data",
-        })
-      );
-
-      const response = await fetch(
-        "https://react-http-35c4a-default-rtdb.firebaseio.com/cart",
-        {
-          //POST는 새 데이터를 데이터 목록에 추가
-          //PUT은 새 데이터를 기존 데이터에 오버라이드
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data filed.");
-      }
-
-      //데이터 바로 가져다 쓸 일은 없으니 굳이 안가져와도 됨
-      //const responseData = await response.json();
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data successfully!",
-        })
-      );
-    };
-
-    //사이드 이펙트가 처음 실행될 때, 즉 앱 처음 로딩 시 장바구니 데이터 전송 차단하기
     if (isInitial) {
-      //이제 첫 렌더링 아니니까 바꿔주고
       isInitial = false;
       return;
     }
 
-    //sendCartData 함수는 promise를 반환하므로 catch를 불러올 수 있음
-    //catch()로 여기에서 발생할 수 있는 모든 오류 포착
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart data filed!",
-        })
-      );
-    });
-
-    //리액트 리덕스가 dispatch를 절대 변경되지 않는 함수임을 보장하므로 의존성 배열에 안전하게 추가 가능
+    //여기서 sendCartData함수, 즉 사용자 지정 액션 생성자에 cart를 인자로 보내기
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const initialCartState = {
   items: [],
@@ -64,6 +65,77 @@ const cartSlice = createSlice({
     },
   },
 });
+
+//RTK는 리듀서 메소드에 대해 이런 액션 생성자를 자동으로 생성하기 때문에 우리가 직접 작성할 필요가 없지만
+//사용자 지정 액션 생성자는 직접 만들어야 한다. 다른 함수를 반환하는 함수를 작성하기 위해서이다.
+
+//지정 액션 생성자 만들기: 슬라이스 갹에 외부에서 새 함수를 생성
+//인자로는 카트 데이터가 필요
+export const sendCartData = (cart) => {
+  //dispatch를 인자로 받아 액션 객체 반환
+  //async비동기 함수로 변환
+  return async (dispatch) => {
+    //첫번째 알람 발송
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data",
+      })
+    );
+
+    // data fetch
+    //if check 전달한다면 잠재적인 오류 처리시 실제로 새 함수를 만들고 여기에 비동기 요청을 보낼 수 있다.
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-http-35c4a-default-rtdb.firebaseio.com/cart.json",
+        {
+          //POST는 새 데이터를 데이터 목록에 추가
+          //PUT은 새 데이터를 기존 데이터에 오버라이드
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data filed.");
+      }
+
+      //데이터 바로 가져다 쓸 일은 없으니 굳이 안가져와도 됨
+      //const responseData = await response.json();
+    };
+
+    try {
+      //sendRequest가 비동기 함수이기 때문에 여기에 호출하고
+      //요청을 보내고 기다릴 수 있고
+      //프로미스도 반환 받을 수 있음
+      await sendRequest();
+      //그리고 현재 비동기 외부 함수인 sendCartData 안에 있기 때문에 여기서도 await 사용가능
+      //함수가 꽤 많이 중첩되어 있지만 API 가져오는 방법 때문에 여기서는 추가적인 중첩이 필요함
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data filed!",
+        })
+      );
+    }
+  };
+};
+// 이 코드는 리듀서에서 실행되는 코드가 아니다. 별도의 독립형 JS 함수임 ㅇㅇ~
+//이 경우 App.js로 이동하여 디스패치 작업을 캡처하여 디스패치하고 알림을 표시하고 복사하고 cartSlice로 이동하여 여기서 실행함
+
+//이렇게 함수를 생성하고 카트 데이터를 보낼 수 있다.
+//이 데이터는 다른 작업을 수행하지 않고 다른 함수인 비동기 함수를 즉시 반환한다.
 
 export const cartActions = cartSlice.actions;
 export default cartSlice.reducer;
